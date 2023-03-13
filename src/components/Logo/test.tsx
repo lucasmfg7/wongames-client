af6339ef5327a0a6b6b78f62a975b04207376f2c
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-03-13 14:12:33 -0300

    Logo Parte 5: Criando primeiros testes

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -18,13 +18,36 @@ describe('<Logo />', () => {
 
 describe('<Logo />', () => {
   it('should render a black label when color is passed', () => {
-    // 1 - Renderizar o componente `render`
-    // 2 - Selecionar o elemento a ser testado `screen` (queries) - getByLabel, getByText
-    // 3 - Expect - assertion - comparação (espero que renderize a logo branca)
-
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
+    })
+  })
+})
+
+describe('<Logo />', () => {
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+})
+
+describe('<Logo />', () => {
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+})
+
+describe('<Logo />', () => {
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
